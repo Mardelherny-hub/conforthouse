06f79991184147a9d89b8c727a2d19e247de0d37
Victor Alcalde <79488170+Mardelherny-hub@users.noreply.github.com>
2026-09-11 01:53:30 -0300

    feat: preparar configuración GA4 y Google Ads

--- config/services.php
+++ config/services.php
@@ -40,4 +40,13 @@ return [
         'secret_key' => env('RECAPTCHA_SECRET_KEY'),
     ],
 
+    'google_analytics' => [
+        'measurement_id' => env('GA4_MEASUREMENT_ID'),
+    ],
+
+    'google_ads' => [
+        'conversion_id' => env('GOOGLE_ADS_CONVERSION_ID'),
+        'conversion_label' => env('GOOGLE_ADS_CONVERSION_LABEL'),
+    ],
+
 ];
