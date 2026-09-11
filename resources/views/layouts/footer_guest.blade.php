c07bf1d6648c2eefb2a27e51157d44de04f12d44
Victor Alcalde <79488170+Mardelherny-hub@users.noreply.github.com>
2026-09-11 01:55:11 -0300

    feat: cargar Google tag en páginas públicas

--- resources/views/layouts/footer_guest.blade.php
+++ resources/views/layouts/footer_guest.blade.php
@@ -1,3 +1,5 @@
+@include('partials.google-tag')
+
 <!-- resources/views/layouts/footer_guest.blade.php -->
 <footer class="bg-neutral-900 text-neutral-300">
     <div class="w-full px-4 lg:px-6 xl:px-8 2xl:px-12 py-12">
@@ -34,7 +36,7 @@
                     <!-- youTube -->
                     <a href="https://www.youtube.com/@ConforthouseLiving" target="_blank"
                     class="w-10 h-10 bg-neutral-800 rounded-full flex items-center justify-center hover:bg-gold-600 transition-colors duration-200" aria-label="LinkedIn">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="currentColor"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 h-5" fill="currentColor"
                                         viewBox="0 0 24 24">
                                         <path
                                             d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
